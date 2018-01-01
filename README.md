663180482599801dd5a0e1b36d4724a1e1c6574a
Isaak Eriksson <isaak.eriksson@gmail.com>
2018-01-01 15:27:16 +0100

    Day 4: Added screenshots and sample settings.json and log.md

---
+++ README.md
@@ -0,0 +1,36 @@
+# Introduction
+
+DevLogger is the fastest command line tool for anyone that wants to update their daily log on Twitter and GitHub: In addition it takes screenshots and is super easy and simple to use from the terminal.
+
+### How to use:
+
+Usage: `python main.py [-h] [--append] [--offline] message`
+
+If you need more detailed information just type `python main.py -h`
+
+# Development
+
+It is under early development and requires the following dependencies:
+
+- gitpython
+- twitter
+- mss
+
+Which can be installed using `sudo pip install gitpython twitter mss`
+
+# Installation
+
+In order to run the script you need to set up a settings.json file. It contains your password and username for Twitter and other sensitive information.
+
+An example `settings.json` file:
+
+"""
+{
+	"username":"user",
+	"password":"pass",
+	"logpath":"log.md",
+	"email": "isaak.eriksson@gmail.com",
+	"repo": "url",
+	"gallery", "screenshots"
+}
+"""
--- main.py
+++ main.py
@@ -3,81 +3,112 @@ import os
 import datetime
 import argparse
 import git
+from mss import mss
 
 from twitter.api import Twitter
 
 try:
-    to_unicode = unicode
+  to_unicode = unicode
 except NameError:
-    to_unicode = str
-
-# Setting up repo
-repo = git.Repo(settings['repo'])
+  to_unicode = str
 
 # Get the settings from settings.json
 settings = {}
 with open('settings.json') as outfile:
-    settings = json.load(outfile)
+  settings = json.load(outfile)
 
 # Setup the parser
-parser = argparse.ArgumentParser(description='A simpler faster way to log your development process that tweets and pushes commits with screenshots')
+parser = argparse.ArgumentParser(
+    description='A simpler faster way to log your development process that tweets and pushes commits with screenshots')
 parser.add_argument('message', type=str,
-                   help='text to be commited and tweeted')
+                    help='text to be commited and tweeted')
+parser.add_argument('--append', action="store_true",
+                    help='Commit on the same day as the last commit')
+parser.add_argument('--offline', action="store_true",
+                    help='Only update the readme file, no commit or tweet')
+parser.add_argument('--text', action="store_true",
+                    help="Don't take a screenshot for the latest log")
 
 args = parser.parse_args()
 
-def setupData():
-    data = {
-        'day': 1,
-        'log': []
-    }
+# Setting up repo
+if args.offline is False:
+  repo = git.Repo(settings['repo'])
 
-    # Write JSON file
-    with open('data.json', 'w', encoding='utf8') as outfile:
-        str_ = json.dumps(data,
-                          indent=4, sort_keys=True,
-                          separators=(',', ': '), ensure_ascii=False)
-        outfile.write(to_unicode(str_))
+
+# Initialize the data.json file
+def setupData():
+  data = {
+      'day': 1,
+      'log': [""]
+  }
+
+  # Write JSON file
+  with open('data.json', 'w', encoding='utf8') as outfile:
+    str_ = json.dumps(data,
+                      indent=4, sort_keys=True,
+                      separators=(',', ': '), ensure_ascii=False)
+    outfile.write(to_unicode(str_))
 
 
 if os.path.exists('data.json') is False:
-    setupData()
+  setupData()
 
 data = {}
 with open('data.json') as outfile:
-    try:
-        data = json.load(outfile)
-    except Exception as e:
-        setupData()
-        data = json.load(outfile)
+  try:
+    data = json.load(outfile)
+  except Exception as e:
+    setupData()
+    data = json.load(outfile)
 
 with open('data.json', 'w', encoding='utf8') as outfile:
-	if data['log'] is None:
-		data['log'] = []
+  if data['log'] is None:
+    setupData()
 
-	if data['day'] is None:
-		data['day'] = 1
+  if data['day'] is None:
+    data['day'] = 1
 
-	day = data['day']
+  day = data['day']
+  logTitle = '### Day ' + str(day) + ': ' + \
+      datetime.datetime.now().strftime("%B %d, %A")
 
-	data['log'].append('### Day ' + str(day) + ': ' + datetime.datetime.now().strftime("%B %d, %A") + '\n\n' + args.message)
+  if args.append is False:
+    if data['log'][-1] == "":
+      data['log'][-1] = (logTitle + '\n\n' + args.message)
+    else:
+      data['log'].append(logTitle + '\n\n' + args.message)
+    
+    data['day'] += 1
+  else:
+    data['log'][-1] += ('\n\n' + args.message)
 
-	data['day'] += 1
-	str_ = json.dumps(data,
-                      indent=4, sort_keys=True,
-                      separators=(',', ': '), ensure_ascii=False)
-	outfile.write(to_unicode(str_))
+  # The simplest use, save a screenshot of the 1st monitor
+  # TODO: Use the callback of sct.save with lambda
+  if args.text is False:
+    with mss() as sct:
+      sct.shot(output=settings['gallery'] + '/{date:%Y-%m-%d}.png')
+      data['log'][-1] += "\n\n[![Foo]("+'https://raw.githubusercontent.com/'+settings['username'] +'/'+ settings['reponame'] +'/'+ settings['gallery'] + datetime.datetime.now().strftime("%Y-%m-%d") + ".png)](Screenshot)"
+
+  str_ = json.dumps(data,
+                    indent=4, sort_keys=True,
+                    separators=(',', ': '), ensure_ascii=False)
+  outfile.write(to_unicode(str_))
 
 # Login and send tweet to Twitter
-twitter = Twitter(settings['username'],settings['password'])
-twitter.statuses.update(data['log'][-1]+ ' ' + settings['hashtags'])
+if args.offline is False:
+  twitter = Twitter(settings['username'], settings['password'])
+  twitter.statuses.update(data['log'][-1] + ' ' + settings['hashtags'])
 
-# Update devlog markdown file and commit
+
+# Update devlog markdown file with screenshot and commit
 with open(settings['logpath'], 'w', encoding='utf8') as outfile:
-	str_ = ""
-	for log in data['log']:
-		str_ += log + '\n\n'
+  str_ = ""
 
-	outfile.write(to_unicode(str_))
+  for log in data['log']:
+    str_ += log + '\n\n'
+    
+  outfile.write(to_unicode(str_))
 
-repo.git.commit('-m', message, author=settings['email'])
+if args.offline is False:
+  repo.git.commit('-m', message, author=settings['email'])
