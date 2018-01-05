54db37655754e5265e458577fc3d03f28bbb5608
Isaak Eriksson <isaak.eriksson@gmail.com>
2018-01-05 15:50:02 +0100

    Day 8: Added support for multiple screenshots in a day's log

--- log.md
+++ log.md
@@ -14,3 +14,33 @@ This message has been written from DevLogger
 
 This message has been written from DevLogger without screenshot
 
+### Day 4: January 05, Friday
+
+Worked on removing the anaconda errors
+
+[![Foo](https://github.com/myisaak/DevLogger/raw/master/screenshots/2018-01-05.png)](Screenshot)
+
+### Day 5: January 05, Friday
+
+Worked on removing the anaconda errors
+
+[![Foo](https://github.com/myisaak/DevLogger/raw/master/screenshots/2018-01-05.png)](Screenshot)
+
+### Day 6: January 05, Friday
+
+Worked on removing the anaconda errors
+
+[![Foo](https://github.com/myisaak/DevLogger/raw/master/screenshots/2018-01-05-1515163486.png)](Screenshot)
+
+### Day 7: January 05, Friday
+
+Worked on removing the anaconda errors
+
+[![Foo](https://github.com/myisaak/DevLogger/raw/master/screenshots/2018-01-05-1515163572.png)](Screenshot)
+
+### Day 8: January 05, Friday
+
+Worked on removing the anaconda errors
+
+[![Foo](https://github.com/myisaak/DevLogger/raw/master/screenshots/2018-01-05-1515163582.png)](Screenshot)
+
--- main.py
+++ main.py
@@ -8,18 +8,19 @@ from mss import mss
 from twitter.api import Twitter
 
 try:
-  to_unicode = unicode
+    to_unicode = unicode
 except NameError:
-  to_unicode = str
+    to_unicode = str
 
 # Get the settings from settings.json
 settings = {}
 with open('settings.json') as outfile:
-  settings = json.load(outfile)
+    settings = json.load(outfile)
 
 # Setup the parser
 parser = argparse.ArgumentParser(
-    description='A simpler faster way to log your development process that tweets and pushes commits with screenshots')
+    description='A simpler faster way to log your development' +
+    'process that tweets and pushes commits with screenshots')
 parser.add_argument('message', type=str,
                     help='text to be commited and tweeted')
 parser.add_argument('--append', action="store_true",
@@ -33,82 +34,84 @@ args = parser.parse_args()
 
 # Setting up repo
 if args.offline is False:
-  repo = git.Repo(settings['repo'])
+    repo = git.Repo(settings['repo'])
 
 
 # Initialize the data.json file
 def setupData():
-  data = {
-      'day': 1,
-      'log': [""]
-  }
+    data = {
+        'day': 1,
+        'log': [""]
+    }
 
-  # Write JSON file
-  with open('data.json', 'w', encoding='utf8') as outfile:
-    str_ = json.dumps(data,
-                      indent=4, sort_keys=True,
-                      separators=(',', ': '), ensure_ascii=False)
-    outfile.write(to_unicode(str_))
+    # Write JSON file
+    with open('data.json', 'w', encoding='utf8') as outfile:
+        str_ = json.dumps(data,
+                          indent=4, sort_keys=True,
+                          separators=(',', ': '), ensure_ascii=False)
+        outfile.write(to_unicode(str_))
 
 
 if os.path.exists('data.json') is False:
-  setupData()
+    setupData()
 
 data = {}
 with open('data.json') as outfile:
-  try:
-    data = json.load(outfile)
-  except Exception as e:
-    setupData()
-    data = json.load(outfile)
+    try:
+        data = json.load(outfile)
+    except Exception as e:
+        setupData()
+        data = json.load(outfile)
 
 with open('data.json', 'w', encoding='utf8') as outfile:
-  if data['log'] is None:
-    setupData()
+    if data['log'] is None:
+        setupData()
 
-  if data['day'] is None:
-    data['day'] = 1
+    if data['day'] is None:
+        data['day'] = 1
 
-  day = data['day']
-  logTitle = '### Day ' + str(day) + ': ' + \
-      datetime.datetime.now().strftime("%B %d, %A")
+    day = data['day']
+    logTitle = '### Day ' + str(day) + ': ' + \
+        datetime.datetime.now().strftime("%B %d, %A")
 
-  if args.append is False:
-    if data['log'][-1] == "":
-      data['log'][-1] = (logTitle + '\n\n' + args.message)
+    if args.append is False:
+        if data['log'][-1] == "":
+            data['log'][-1] = (logTitle + '\n\n' + args.message)
+        else:
+            data['log'].append(logTitle + '\n\n' + args.message)
+
+        data['day'] += 1
     else:
-      data['log'].append(logTitle + '\n\n' + args.message)
-    
-    data['day'] += 1
-  else:
-    data['log'][-1] += ('\n\n' + args.message)
+        data['log'][-1] += ('\n\n' + args.message)
 
-  # The simplest use, save a screenshot of the 1st monitor
-  # TODO: Use the callback of sct.save with lambda
-  if args.text is False:
-    with mss() as sct:
-      sct.shot(output=settings['gallery'] + '/{date:%Y-%m-%d}.png')
-      data['log'][-1] += "\n\n[![Foo]("+'https://github.com/'+settings['username'] +'/'+ settings['reponame'] +'/raw/master/'+ settings['gallery'] + datetime.datetime.now().strftime("%Y-%m-%d") + ".png)](Screenshot)"
+    # The simplest use, save a screenshot of the 1st monitor
+    # TODO: Use the callback of sct.save with lambda
+    if args.text is False:
+        with mss() as sct:
+            sct.shot(output=settings['gallery'] + '/{date:%Y-%m-%d-%s}.png')
+            data['log'][-1] += ('\n\n[![Foo](' + 'https://github.com/' +
+                settings['username'] + '/'+ settings['reponame'] +
+                '/raw/master/'+ settings['gallery'] +
+                datetime.datetime.now().strftime("%Y-%m-%d-%s") +
+                ".png)](Screenshot)")
 
-  str_ = json.dumps(data,
-                    indent=4, sort_keys=True,
-                    separators=(',', ': '), ensure_ascii=False)
-  outfile.write(to_unicode(str_))
+        str_ = json.dumps(data,indent=4, sort_keys=True,separators=(',', ': '), ensure_ascii=False)
+        outfile.write(to_unicode(str_))
 
 # Login and send tweet to Twitter
 if args.offline is False:
-  twitter = Twitter(settings['username'], settings['password'])
-  twitter.statuses.update(data['log'][-1] + ' ' + settings['hashtags'])
+    twitter = Twitter(settings['username'], settings['password'])
+    twitter.statuses.update(data['log'][-1] + ' ' + settings['hashtags'])
 
 
 # Update devlog markdown file with screenshot and commit
 with open(settings['logpath'], 'w', encoding='utf8') as outfile:
-  str_ = ""
+    str_ = ""
 
-  for log in data['log']:
-    str_ += log + '\n\n'
-    
-  outfile.write(to_unicode(str_))
+    for log in data['log']:
+        str_ += log + '\n\n'
+
+    outfile.write(to_unicode(str_))
 
 if args.offline is False:
-  repo.git.commit('-m', message, author=settings['email'])
+    repo.git.commit('-m', args.message, author=settings['email'])
